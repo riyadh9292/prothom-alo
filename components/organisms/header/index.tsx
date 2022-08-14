9c15d63e2ecd59d7fee631ffbc702f2ba6b8a43c
riyadh <mrriyadh66@gmail.com>
2022-08-14 23:54:25 +0600

    project-setup done

---
+++ components/organisms/header/index.tsx
@@ -0,0 +1,9 @@
+import { FunctionComponent } from "react";
+
+interface HeaderProps {}
+
+const Header: FunctionComponent<HeaderProps> = () => {
+  return <div className="">header</div>;
+};
+
+export default Header;
